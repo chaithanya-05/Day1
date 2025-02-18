27f80a657f638dd39ffa0c2b370aca4b3113064f
chaithanya-05 <chaithanyas298@gmail.com>
2025-02-18 21:44:53 +0530

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-// Function to print a right-angled triangle of asterisks
+// Function to print a right-angled triangle 
 void printTriangle(int n) {
-    for (int i = 1; i <= n; i++) { // Loop for each row
-        for (int j = 1; j <= i; j++) { // Loop for printing stars in each row
-            printf("* "); // Print a star followed by a space
+    for (int i = 1; i <= n; i++) { 
+        for (int j = 1; j <= i; j++) { 
+            printf("* "); 
         }
-        printf("\n"); // Move to the next line after each row
+        printf("\n"); 
     }
 }
 
